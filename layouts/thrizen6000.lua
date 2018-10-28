ba1de6d42ce7ac9b429b013d12d9487139038624
William Villeneuve <ciiqr@users.noreply.github.com>
2018-10-28 14:12:57 -0400

    some style changes for thrizen

--- layouts/thrizen6000.lua
+++ layouts/thrizen6000.lua
@@ -1,4 +1,6 @@
 
+-- Area is divided into 1-3 even columns, then new rows start after that.
+
 -- Grab environment we need
 local pairs = pairs
 
@@ -6,31 +8,27 @@ local thrizen = {
     name = "thrizen",
 }
 
-function thrizen.arrange(currentScreen)
-    local area = currentScreen.workarea
-    local screenOffsetX = area.x
-    local screenOffsetY = area.y
-
+function thrizen.arrange(screen)
     local desiredColumns = 3
 
-    -- Area is divided into 1-3 even columns
-    -- then new rows start after that...
+    -- Get the offset/size of this screen
+    local screenArea = screen.workarea
 
     -- Get the number of clients
-    local numClients = #currentScreen.clients
+    local numClients = #screen.clients
 
     -- Determine the number of columns (min of numClients and desired columns)
     local numColumns = numClients > desiredColumns and desiredColumns or numClients
     -- Determine the individual client width based on the number of columns
-    local targetWidth = area.width / numColumns
+    local targetWidth = screenArea.width / numColumns
 
     -- Determine the number of rows (must be a whole number)
     local numRows = math.ceil(numClients / numColumns)
     -- Determine the individual client height based on the number of rows
-    local targetHeight = area.height / numRows
+    local targetHeight = screenArea.height / numRows
 
     -- Iterate over the clients
-    for i, c in pairs(currentScreen.clients) do
+    for i, c in pairs(screen.clients) do
         -- Use the current index to determine the current column and row
         local currentColumn = ((i - 1) % numColumns)
         local currentRow = math.floor((i - 1) / numColumns)
@@ -42,9 +40,9 @@ function thrizen.arrange(currentScreen)
         local clientOffsetX = currentColumn * targetWidth
         local clientOffsetY = currentRow * targetHeight
 
-        currentScreen.geometries[c] = {
-            x = screenOffsetX + clientOffsetX,
-            y = screenOffsetY + clientOffsetY,
+        screen.geometries[c] = {
+            x = screenArea.x + clientOffsetX,
+            y = screenArea.y + clientOffsetY,
             width = targetWidth,
             height = isDoubleHeight and 2 * targetHeight or targetHeight
         }
