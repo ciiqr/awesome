8af7a08070d1d4d975e95783247b62a7d6ae1f01
William Villeneuve <ciiqr@users.noreply.github.com>
2019-02-06 19:54:11 -0500

    out damn space

--- widgets/battery.lua
+++ widgets/battery.lua
@@ -19,7 +19,7 @@ local function customWrapper(format, warg)
     -- On Battery
     if retval[1] == "−" or retval[1] == "-" then
         local function notifyBatteryWarning(level)
-            notifySend(level.." Battery: "..batteryPercent.."% !", 0, naughty.config.presets.critical)
+            notifySend(level.." Battery: "..batteryPercent.."%!", 0, naughty.config.presets.critical)
         end
         -- Low Battery
         -- TODO: make this less dumb about getting config
