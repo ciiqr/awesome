da47e908acc1b9eb3dec9ce232fcca7eb977baf7
William Villeneuve <ciiqr@users.noreply.github.com>
2020-05-13 21:22:12 -0400

    disabling this for now

--- rules/rules.lua
+++ rules/rules.lua
@@ -25,8 +25,8 @@ return {
             maximized = false,
             maximized_vertical   = false,
             maximized_horizontal = false,
-            -- Prevent Clients preserving their tags on Start
-            tags = {""},
+            -- TODO: Prevent Clients preserving their tags on Start, below doesn't work mostly because it breaks on awesome restart
+            -- tags = {""},
             -- Perfect, I never have to worry about this crap again!
             size_hints_honor = false,
             -- awesome docs suggests these...
